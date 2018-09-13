1f5e23d5546944f7986b743931cb662f47035f4b
Julio Isabel Martinez Leal <julio.leal@neoris.cxnetworks.net>
2018-09-13 11:07:54 -0500

    Primer Commit

---
+++ TestCustomVision/TestCustomVision.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace TestCustomVision.Droid
+{
+    [Activity(Label = "TestCustomVision", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Acr.UserDialogs.UserDialogs.Init(this);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ TestCustomVision/TestCustomVision/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System.Net.Http;
+using Acr.UserDialogs;
+
+namespace TestCustomVision
+{
+    public partial class MainPage : ContentPage
+    {
+        private MediaFile _foto;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ElegirClick(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            var foto = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions());
+
+            _foto = foto;
+            ImgSource.Source = FileImageSource.FromFile(foto.Path);
+        }
+
+        private async void TomarFoto(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            var foto = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+            {
+                Directory = "clasificador",
+                Name = "sourse.jpg"
+            });
+            _foto = foto;
+            ImgSource.Source = FileImageSource.FromFile(foto.Path);
+        }
+
+        private async void AnalizarFoto(object sender, EventArgs e)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Prediction-Key", "d20c03142343439d8598d1cf03558421");
+            const string endpoint = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/1cd65429-17d7-4a80-a31e-a57023de206f/image?iterationId=3daf1c6d-2b6a-4548-a826-2ed598188d94";
+
+            var contentStream = new StreamContent(_foto.GetStream());
+
+            var response = await httpClient.PostAsync(endpoint, contentStream);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                UserDialogs.Instance.Toast("A ocurrido un error");
+                return;
+            }
+
+            var jason = await response.Content.ReadAsStringAsync();
+
+
+        }
+    }
+}
